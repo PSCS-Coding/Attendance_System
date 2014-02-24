8ee97d677c04e5e5c383d538f17fb5442cd111be
Milo Rupp <milo.rupp@pscs.org>
2014-02-24 15:04:19 -0800

    added a test table

--- attendance.php
+++ attendance.php
@@ -5,10 +5,28 @@
 
 <body>
 
-<h3>Student</h3>
-<h3>Status</h3>
-<h3>Comment</h3>
-
+<table style="width:80%">
+    <tr>
+    <th>Student</th>
+    <th>Status</th>
+    <th>Comment</th>
+    </tr>
+    <tr>
+        <td>Anne</td>
+        <td>Bonnie</td>
+        <td>Clara</td>
+    </tr>
+    <tr>
+        <td>Present</td>
+        <td>Present</td>
+        <td>Present</td>    
+    </tr>
+    <tr>
+        <td>5:00</td>
+        <td>9:30</td>
+        <td>1:11</td>
+    </tr>
+</table>
 </body>
 
 </html>
