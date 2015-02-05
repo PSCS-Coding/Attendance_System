85c76fbb9afa08107fc272d5ac18a3144b9020fe
riverehill <riverehill@gmail.com>
2015-02-05 14:11:20 -0800

    changed editing-row color to darker grey

--- a/pages/edit_events.php
+++ a/pages/edit_events.php
@@ -208,7 +208,7 @@ if (!empty($_POST['studentselect'])) {
                   $timestamp_to_edit = $event['timestamp']; // Capture this to pass to the JS timepicker below                 
             ?>
             <form method='post' name='inline_edit' action='<?php echo basename($_SERVER['PHP_SELF']); ?>?id=<?echo $current_student_id?>&eventid=<?echo $event['eventid']?>'>
-              <tr class="editing-row" style="background-color: orange;">
+              <tr class="editing-row" style="background-color: rgb(121, 121, 121);">
                   <td><?php echo $event['eventid'] ?></td>
                   <td>
                      <input type='text' id='stamp_edit' name='stamp_edit' value='<?php echo $event['timestamp']; ?>'>
