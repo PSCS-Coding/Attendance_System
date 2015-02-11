363fe3e3a51896534fd1dc8cbde22ebb601e1fb2
riverehill <riverehill@gmail.com>
2015-02-11 11:22:38 -0800

    fixed button stacking issue

--- index.php
+++ index.php
@@ -464,7 +464,7 @@
 						<?php } ?>
 
 					</td>
-					<td class='status_col' style="padding-right: 30%;"><?php 
+					<td class='status_col'><?php 
 						$returntimeobject = new DateTime($latestdata['returntime']);
 						echo $latestdata['statusname'] . " "; 
 						if ($latestdata['statusname'] == "Offsite") {
