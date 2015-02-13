a39935480c3e7c2e21368e639dbe6b9c97d4b921
Appleguy15 <anthonyreyes@Disk-1.local>
2015-02-13 11:03:26 -0800

    Fixed code error
    When you had something in the cookie it would let you through.

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
         <?php
-        if (isset($_COOKIE["loginV2"])) {
+    if (!empty($_COOKIE["loginV2"])) {
   
             if (password_verify('admin12873912', $_COOKIE["loginV2"])) {
                 
@@ -15,6 +15,12 @@
             
             
         } else {
+echo '<META http-equiv="refresh" content="0;URL=secondary_login.php">';
+}
+// If login is not equal to student or admin redirect
+if (password_verify('admin12873912', $_COOKIE["loginV2"]) || password_verify('student87162387', $_COOKIE["loginV2"])) {
+// Leave Empty    
+}else {
 echo '<META http-equiv="refresh" content="0;URL=secondary_login.php">';
 }
         ?>
