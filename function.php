23a0cc7a4e61c3562e1cdcfaa2fe2158d83dadd3
Milo Rupp <milo.rupp@pscs.org>
2015-02-26 15:22:31 -0800

    added vr calculations to function.php

--- function.php
+++ function.php
@@ -310,4 +310,213 @@ function statconvert($id)
     $name = $tempvar['statusname'];
     return($name);
 }
+
+function calculateStats($current_student_id) //======================== passes out an array of view-reports type information ====================================
+{
+	
+global $db_server;
+//holidays array
+$holiday_data_array = array();
+$holiday_dt_array = array();
+$holidayquery = "SELECT date FROM holidays";
+$holiday_result = $db_server->query($holidayquery);
+
+while ($holiday_data = $holiday_result->fetch_array()) {
+	array_push($holiday_dt_array, $holiday_data);
+}
+foreach ($holiday_dt_array as $k) {
+	array_push($holiday_data_array, $k[0]);
+}
+if (isset($current_student_id)) {
+$student_data_array = array();
+//fetches most recent data from the events table
+//joins with the tables that key student names/status names to the ids in the events table
+$result = $db_server->query("SELECT info,statusname,studentdata.studentid,studentdata.firstname,timestamp,returntime,events.eventid, yearinschool
+		FROM events
+		JOIN statusdata ON events.statusid = statusdata.statusid
+		RIGHT JOIN studentdata ON events.studentid = studentdata.studentid
+		WHERE studentdata.studentid = $current_student_id
+		ORDER BY timestamp ASC") or die(mysqli_error($db_server));
+while ($student_data_result = $result->fetch_assoc()) {
+	array_push($student_data_array, $student_data_result);
+}
+
+//allotted hours query
+$yearinschool = $student_data_array[0]['yearinschool'];
+$allottedquery = "SELECT * FROM allottedhours WHERE yis = '$yearinschool'";
+$allotted_result = $db_server->query($allottedquery);
+$allotted_data_array = $allotted_result->fetch_array();
+
+//globals query
+$globalsquery = "SELECT * FROM globals";
+$globals_result = $db_server->query($globalsquery);
+$globalsdata = $globals_result->fetch_array();
+
+$starttime = $globalsdata['starttime'];
+$endtime = $globalsdata['endtime'];
+$startdate = $globalsdata['startdate'];
+$lastdate = $globalsdata['enddate'];
+
+$study_all = $allotted_data_array['IShours'] * 60;
+$offsite_all = $allotted_data_array['offsitehours'] * 60;
+$commhours_all = $allotted_data_array['communityhours'] * 60;
+
+$studyhours_remaining = $study_all;
+$offsitehours_remaining = $offsite_all;
+$commhours_remaining = $commhours_all;
+
+$query = $db_server->query("SELECT yearinschool FROM studentdata WHERE studentid = $current_student_id");
+$tempvar = $query->fetch_assoc();
+$studentYis = $tempvar['yearinschool'];
+
+$query = $db_server->query("SELECT startdate FROM studentdata WHERE studentid = $current_student_id");
+$tempvar = $query->fetch_assoc();
+$studentStartDate = $tempvar['startdate'];
+
+$startDateStamp = new DateTime($studentStartDate);
+$yearDateStamp = new DateTime($startdate);
+
+if ($startDateStamp > $yearDateStamp){ // if a student is mid-year, then nerf the number of IS and offsite hours they have
+$remainingDays = daysLeftFromDate($studentStartDate);
+$totalDays = daysLeftFromDate($startdate);
+
+$baseHours = $remainingDays / $totalDays;
+$baseISHours = $remainingDays / $totalDays;
+$totalISHours = $study_all / 60;
+$totalHours = $offsite_all / 60;
+$baseHours = $baseHours * $totalHours;
+$baseISHours = $baseISHours * $totalISHours;
+
+$offsitehours_used = 0;
+$commhours_used = -$baseHours * 60;
+$studyhours_used = 0;
+$studyhours_remaining = $study_all - $baseISHours * 60;
+$offsitehours_remaining = $offsite_all - $baseHours * 60;
+$commhours_remaining = $commhours_all - $baseHours * 60;
+
+} else { // if not a new student, do nothing
+	$offsitehours_used = 0;
+	$commhours_used = 0;
+	$studyhours_used = 0;
+	$studyhours_remaining = $study_all;
+	$offsitehours_remaining = $offsite_all;
+	$commhours_remaining = $commhours_all;
+}
+
+$num_lates = 0;
+$num_unexpected = 0;
+$num_absent = 0;
+
+$offsiteremaining = $offsitehours_remaining / 60;
+//counts time
+//loops through each event for the given student
+foreach($student_data_array as $event_key => $event_val) {
+	if ($student_data_array[$event_key]['statusname'] != "Not Checked In") {
+	if (count($student_data_array) != $event_key) {
+		//makes the timestamp into datetime obj
+		if (isset($student_data_array[$event_key+1])) {
+			$event_datetime_2 = new DateTime($student_data_array[$event_key+1]['timestamp']);
+		}
+		else {
+			break;
+		}
+		$event_datetime_1 = new DateTime($event_val['timestamp']);
+		//variables for easy comparison
+		$early = $event_datetime_1->format('m/d/y') . " " . $starttime;
+		$late = $event_datetime_1->format('m/d/y') . " " . $endtime;
+		$event_early = new DateTime($early);
+		$event_late = new DateTime($late);
+		//is event 1 on a weekend or holiday?
+		if (($event_datetime_1->format('w') == 0 || $event_datetime_1->format('w') == 6) || (in_array($event_datetime_1->format('Y-m-d'), $holiday_data_array) == True)) {
+			continue;
+		}
+		//is event 1 before 9:00am?
+		if ($event_datetime_1 <= $event_early){
+			$event_datetime_1 = clone $event_early;
+		}
+		//is event 2 after 3:30 of the same day of event 1?
+		if ($event_datetime_2 >= $event_late){
+			$event_datetime_2 = clone $event_late;
+		}
+		
+		if ($event_datetime_2 > $event_datetime_1) { // without this logic, you can end up with (adjusted) event 1 coming *after* event 2!  (For example, a "checked out" event at 3:45 will result in a 15 minute diff.)
+			$elapsed = $event_datetime_2->diff($event_datetime_1);
+		} else {
+			$elapsed = $event_datetime_1->diff($event_datetime_1);
+		}
+		//format as total minutes
+		$elapsed_minutes = ($elapsed->format('%h')*60) + ($elapsed->format('%i'));
+		
+		if ($event_val['statusname'] == 'Present' || $event_val['statusname'] == 'Field Trip' || $event_val['statusname'] == 'Independent Study') {
+			$commhours_remaining -= $elapsed_minutes;
+			$commhours_used += $elapsed_minutes;
+			if ($event_val['statusname'] == 'Independent Study') {
+				$studyhours_remaining -= $elapsed_minutes;
+				$studyhours_used += $elapsed_minutes;
+			}
+		}
+		else {
+			$offsitehours_remaining -= $elapsed_minutes;
+			$offsitehours_used += $elapsed_minutes;
+			if (($event_val['statusname'] == 'Not Checked In') && ($event_datetime_1 >= new DateTime($starttime))) {
+				$num_unexpected += 1;
+				$num_lates += 1;
+			}
+			if ($event_val['statusname'] == 'Late') {
+				$num_lates += 1;
+			}
+			if ($event_val['statusname'] == 'Absent') {
+				$num_absent += 1;
+				}
+			}
+		}
+	}
+}
+$offsiteHrs_remaining = floor($offsitehours_remaining / 60);
+$offsiteMin_remaining = $offsitehours_remaining % 60;
+
+$objstarttime = new DateTime($starttime);
+$objendtime = new DateTime($endtime);
+$daydiff = $objstarttime->diff($objendtime);
+$minutesinday = $daydiff->format('%i');
+$hoursinday = $daydiff->format('%h');
+$hoursinday = $hoursinday * 60;
+$totalminsinday = $hoursinday + $minutesinday;
+$totalminsinday = $totalminsinday / 60;
+
+$fulldaysleft = floor($offsitehours_remaining / $minutesinday);
+
+$returnArray = array();
+array_push($returnArray,$offsiteHrs_remaining,$offsiteMin_remaining);
+
+
+$offsiteHrs_used = floor(($offsitehours_used) / 60);
+$offsiteMin_used = $offsitehours_used % 60;
+
+// this uses a function from functions.php
+$daystillend = daysLeft();
+
+if ($daystillend > 0) {
+$minutesperday = floor($offsitehours_remaining / $daystillend);
+array_push($returnArray,$minutesperday);
+} else {
+array_push($returnArray,null);
+}
+
+$daysInYear = daysLeftFromDate($globalsdata['startdate']);
+
+$yearPercent = floor(100 - ($daystillend / $daysInYear * 100));
+
+$offsitePercent = floor($offsiteHrs_used / $offsiteremaining * 100);
+
+array_push($returnArray,$offsitePercent);
+	
+}
+
+
+
+
+
+
+
 ?>
