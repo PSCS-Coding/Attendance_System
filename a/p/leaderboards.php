9414b506ab6a45dce34c6534a97ddd4d9cbdc838
Milo Rupp <milo.rupp@pscs.org>
2015-03-02 13:03:19 -0800

    it tables

--- a/p/leaderboards.php
+++ a/p/leaderboards.php
@@ -7,32 +7,47 @@
 <?php 
 $studentData = $db_server->query("SELECT * FROM studentdata WHERE current = 1 ORDER BY firstname");
 $studentTable = array();
+$studentNames = array();
  while ($studentRow = mysqli_fetch_assoc($studentData)) {
 	$currentStudentRow = array();
-	$studentNames = array();
 	$lastInit = $studentRow['lastname'][0];
 	$stats = calculateStats($studentRow['studentid']);
 	array_push($currentStudentRow,$studentRow['firstname'],$lastInit,$stats[2]);
-	array_push($studentNames,$stats[2]);
 	array_push($studentTable,$currentStudentRow);
+	array_push($studentNames,$studentRow['firstname']);
 }
 
-foreach ($studentTable as $key => $row)
-{
+foreach ($studentTable as $key => $row){
     $studentTable[2][$key]  = $row[2];
 }    
 
-
 array_multisort($studentTable[2], SORT_ASC, $studentTable);
 
-?>
-<br>
-<?php 
-foreach ($studentTable as $render){
-		echo $render[2];
-		?> <br> <?php
+for ($i = 1; $i < count($studentTable); $i++){
+	
+		if(!in_array($studentTable[$i][0],$studentNames)){
+			unset($studentTable[$i]); 
+		}
 }
 
 ?>
+<br>
+<table>
+<tr> 
+<th> name </th>
+<th> minutes per day </th>
+</tr>
+<?php 
+foreach ($studentTable as $render){
+	?> 
+	<tr>
+	<td> <?php echo $render[0]; ?> </td>
+	<td> <?php echo $render[2]; ?> </td>
+	</tr>
+<?php
+}
+
+?>
+</table>
 </body>
 </html>
