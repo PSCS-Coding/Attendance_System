e9de8ba4fa4f81b2cdde3ad3c180c40bd5aa4c53
Milo Rupp <milo.rupp@pscs.org>
2015-03-02 11:48:54 -0800

    ordered the list mostly right and removed htaccess

--- a/p/leaderboards.php
+++ a/p/leaderboards.php
@@ -1,14 +1,38 @@
 <html>
 <head>
-	<title>Edit Students</title>
+	<title>Leaderboards</title>
 	<?php require_once('header.php'); ?>
 </head>
 <body>
 <?php 
 $studentData = $db_server->query("SELECT * FROM studentdata WHERE current = 1 ORDER BY firstname");
+$studentTable = array();
  while ($studentRow = mysqli_fetch_assoc($studentData)) {
-	print_r($studentRow);
+	$currentStudentRow = array();
+	$studentNames = array();
+	$lastInit = $studentRow['lastname'][0];
+	$stats = calculateStats($studentRow['studentid']);
+	array_push($currentStudentRow,$studentRow['firstname'],$lastInit,$stats[2]);
+	array_push($studentNames,$stats[2]);
+	array_push($studentTable,$currentStudentRow);
 }
+
+foreach ($studentTable as $key => $row)
+{
+    $studentTable[2][$key]  = $row[2];
+}    
+
+
+array_multisort($studentTable[2], SORT_ASC, $studentTable);
+
+?>
+<br>
+<?php 
+foreach ($studentTable as $render){
+		echo $render[2];
+		?> <br> <?php
+}
+
 ?>
 </body>
 </html>
