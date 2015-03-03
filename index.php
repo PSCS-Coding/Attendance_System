681544e7b2ddd297469449cb87a88f2b18de0f52
riverehill <riverehill@gmail.com>
2015-03-03 12:00:24 -0800

    added delay to tooltip

--- index.php
+++ index.php
@@ -25,7 +25,12 @@
 		</script>
 		<script>
 			$(function() {
-			    $(document).tooltip();    
+			    $(document).tooltip({
+				show: {
+				effect: "slideDown",
+				delay: 1500
+				}	
+			});    
 			});
 		    </script>
 		<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
