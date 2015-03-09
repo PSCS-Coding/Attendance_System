c2aa9b198c1faed7628edbe2b838289534b63f76
riverehill <riverehill@gmail.com>
2015-03-09 09:38:41 -0700

    added jQuery to range select with shift key

--- index.php
+++ index.php
@@ -529,5 +529,37 @@
 	</table>
 	</div>
 	
+	<!-- CODE FOR MULTI-CHECKBOX SELECT-->
+	
+	<script>
+	    $.fn.shiftSelectable = function() {
+		var lastChecked,
+		    $boxes = this;
+	     
+		$boxes.click(function(evt) {
+		    if(!lastChecked) {
+			lastChecked = this;
+			return;
+		    }
+	     
+		    if(evt.shiftKey) {
+			var start = $boxes.index(this),
+			    end = $boxes.index(lastChecked);
+			$boxes.slice(Math.min(start, end), Math.max(start, end) + 1)
+			    .prop('checked', lastChecked.checked)
+			    .trigger('change');
+		    }
+	     
+		    lastChecked = this;
+		});
+	    };
+	</script>
+	
+	<script>
+	    $(document).ready(function() {
+		$('.c_box').shiftSelectable();    
+	    });
+	</script>
+	
 	</body>
 	</html>
