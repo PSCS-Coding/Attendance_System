829434e615fdba9d297620605831aa391401e18e
Nic Warmenhoven <nic@pscs.org>
2015-03-16 09:48:34 -0700

    Removed legacy link from edit_events

--- a/p/edit_events.php
+++ a/p/edit_events.php
@@ -112,9 +112,9 @@ if (!empty($_GET['id'])) {
               <h1 class="Myheader">Edit Events</h1>
                 </div>
             <div align="center" id="main">
-   <div class="centerr" style="margin: 1em;">
+  <!-- <div class="centerr" style="margin: 1em;">
       <a href="../" style="color: #fff;">Temporary link to main admin interface</a>
-   </div>
+   </div>-->
    <div class="centerr">
       <form method='post' id='studentform' class='studentselect' action='<?php echo basename($_SERVER['PHP_SELF']); ?>'>
          <select name='studentselect'>
