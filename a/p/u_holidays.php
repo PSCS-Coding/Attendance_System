55af98f50ace57f716c33d71d9dafe0fe15fda9f
Anthony <anthony.reyes@pscs.org>
2015-07-13 11:09:48 -0700

    Revamping Update Holidays

--- a/p/u_holidays.php
+++ a/p/u_holidays.php
@@ -1,120 +1,173 @@
 <?php
 $admin = 1;
 require_once('../../login.php');
+/////////////////////////////////////////////
+//                                         //
+//         CREATED BY ANTHONY REYES        //
+//       Puget Sound Community School      //
+//                                         //
+/////////////////////////////////////////////
 ?>
+<!DOCTYPE html>
 <html>
 <head>
-    <title>Edit Holidays</title>
-    <?php require_once('header.php'); ?>
+	<title>Example Page - Students</title>
+	<?php require_once('header.php'); ?>
 </head>
-<body class="adminpage edit-holidays">
-<!-- UPDATE FUNCTIONS -->
+    
+<body class="admin">
+    
+    <!-- HEADER BAR -->
+    <div id="TopHeader"><h1>Update Holidays</h1></div>
+    
 <?php
-// Header Info
-$HeaderStatus = null;
-$HeaderInfo = "Update Holidays";
-// ADD A NEW HOLIDAY
-if (isset($_POST['addnewholiday'])) {
-$date = strtotime($_POST['date']);
-$stmt = $db_server->prepare("INSERT INTO holidays (holidayname, date) VALUES (?, FROM_UNIXTIME(?))");
-$stmt->bind_param('ss', $_POST['holidayname'] , $date);
-$stmt->execute();
-$stmt->close();
-$HolidayName = $_POST['holidayname'];
-$HeaderStatus = "Sussess";
-$HeaderInfo = "Sussessfully added holiday: $HolidayName";
+
+// In-Code Refrences:
+// B = Button
+// NN = New Name
+// Y - Year in school
+// U - Update
+// NN - New Name
+// d_ - Deactivated
+// a - admin
+// hname- Holiday Name
+// hdate - Holiday Date
+
+//MYSQLI SELECT QUERY
+$query_results = $mysqli->query("SELECT * FROM holidays ORDER BY date");
+
+/////// INSERT FUNCTION //////////
+// CHECKING IF THE "ADD STUDENT" BUTTON HAS BEEN CLICKED
+if (!empty($_POST['addnew'])) {
+    
+//VALUES TO BE INSERTED INTO THE STUDENT DATA TABLE
+$new_hname = '"'.$mysqli->real_escape_string('-Example').'"';
+$new_hdate = '"'.$mysqli->real_escape_string('2014-11-10').'"';
+
+//QUERY DEFINING WHAT TO INSERT
+$insert_row = $mysqli->query("INSERT INTO holidays (holidayname, date) VALUES($new_hname, $new_hdate)");
+
+// SUCCESS/ERROR MESSAGES
+if($insert_row){print 'Success!'; }else{die('Error : ('. $mysqli->errno .') '. $mysqli->error);}
+
+// CLOSING FOR ORIGIN IF STATEMENT
 }
 
-// EDIT (UPDATE) A HOLIDAY
-if (isset($_POST['saveholiday'])) {
- $date = strtotime($_POST['date']);
- $stmt = $db_server->prepare("UPDATE holidays SET holidayname = ? , date = FROM_UNIXTIME(?) WHERE id = ?");
-      $stmt->bind_param('ssi', $_POST['holidayname'], $date, $_POST['id']);
-      $stmt->execute();
-      $stmt->close();
-    }
+// CHECKING IF THE "SAVE" BUTTON HAS BEEN CLICKED
+if (!empty($_POST['Save'])) {
+    
+// DEFINING POST VARIABLES
+$u_hname = $_POST['U_holidayname'];
+$u_hdate = $_POST['U_date'];
+$find_id = $_POST['hid'];
 
-// DELETE A HOLIDAY
-if(isset($_POST['deleteholiday'])) {
-    $stmt = $db_server->prepare("DELETE FROM holidays WHERE id = ?");
-    $stmt->bind_param('i', $_POST['id']);
-    $stmt->execute();
-    $stmt->close();
-$HeaderStatus = "Error";
-$HeaderInfo = "Deleted Holiday.";
-    }
+// QUERY DEFINING WHAT TO UPDATE
+$query = "UPDATE holidays SET holidayname = ? , date = ? WHERE id = ?";
+    
+// PREPARE STATEMENT    
+$statement = $mysqli->prepare($query);
+
+//BIND parameters for markers
+$results =  $statement->bind_param('ssi', $u_hname, $u_hdate, $find_id);
+$statement->execute();
+$statement->close();
+// PRINTING SUSSESS OR ERROR
+if($results){print 'Success! record updated'; }else{print 'Error : ('. $mysqli->errno .') '. $mysqli->error;}
+
+// CLOSING ORIGIN IF STATEMENT   
+}
 
 
+////////DELETE FUNCTION/////////
+if (!empty($_POST['Delete'])) {
 
-// GET THE LIST OF HOLIDAYS
-    $holidayresult = $db_server->query("SELECT * FROM holidays ORDER BY date");
-     ?>
-                <div id="TopHeader" class="<?php echo $HeaderStatus; ?>">
-              <h1 class="Myheader"><?php echo $HeaderInfo; ?></h1>
-                </div>
-            <div align="center" id="main">
-<div class="admintable">
-<form style="margin-bottom:1em;" action="" method="post">
-    <input type="text" name="holidayname" placeholder="Holiday Name" required size="15">
-    <input type="text" name="date" id="date" placeholder="Holiday Date" required size="15">
-    <input type="submit" name="addnewholiday" value="Add Holiday" />
-</form>
+// PUTTING POST INTO A VARIABLE FOR QUERY
+$holiday_id = $_POST['hid'];
 
- <table class="holiday_table">
-   <tr>
-      <th>Holiday Name</th>
-      <th>Date</th>
-      <th>Edit</th>
-      <th>Delete</th>
-   </tr>
-<?php
-// Make list of holidays
-while ($list = mysqli_fetch_assoc($holidayresult)) { ?>
+//MYSQLI UPDATE(REMOVE) QUERY
+$results = $mysqli->query("DELETE studentdata SET current='0' WHERE facilitatorid = $student_id");
+}
 
-<form action="" method="post">
-<input type="hidden" name="id" value="<?php echo $list['id']; ?>">
-    <tr>
-        <?php $editme = "edit-" . $list['id'];
-        if (isset($_POST[$editme])) {
-        $adjusteddate = new DateTime($list['date']);
-        ?>
-        <td><input type="text" name="holidayname" class="textbox" value="<?php echo $list['holidayname']; ?>" required size="15"></td>
-        <td><input type="text" name="date" class="textbox" id="editdate" value="<?php echo $adjusteddate->format('m-d-Y'); ?>" required size="15"></td>
-        <td><button type="submit" name="saveholiday" value="<?php echo $list['id']; ?>">Save</button></td>
-        <?php } else { ?>
-        <td><?php echo $list['holidayname']; ?></td>
-        <td><?php echo $list['date']; ?></td>
-
-        <td><input type="submit" name="edit-<?php echo $list['id']; ?>" value="Edit"></td>
-        <?php } ?>
-        <td><button type="submit" name="deleteholiday" value="<?php echo $list['holidayname']; ?>">Delete</button></td>
-    </tr>
-</form>
-<?php
-} // end while
 ?>
+        
+<!-- Start of main table -->
+<table class="center">
+    <th>Holiday</th>
+    <th>Date</th>
+    <th class="textcenter">Options</th>
+
+<?php
+
+// PUTTING SQL RESULTS INTO AN ARRAY
+while($row = $query_results->fetch_array()) {
+        
+    // MAKING A SINGLE VAR FROM POST AND HOLIDAY ID
+    $editMode = "Update" . $row['id'];
+    
+    // CHECKING IF THERE IS POST DATA FOR $editMode
+    if (empty($_POST[$editMode])) {
+    
+        // PRINTING TABLE ROW
+            print '<tr>';
+        // MAKING FORM
+            print '<form action="u_holidays.php" method="POST">';
+        // GETS/MAKES HIDDEN HOLIDAY ID
+            print '<input type="hidden" name="hid" value="'.$row["id"].'">';
+        // PRINTS HOLIDAY NAME
+            print '<td>'.$row["holidayname"].'</td>';
+        // PRINTS HOLIDAY DATE
+            print '<td>'.$row["date"].'</td>';
+        // PRINTS UPDATE BUTTONS
+            print '
+            <td class="textcenter">
+                <input type="submit" class="adminbtn" name="Update'.$row["id"].'" value="Update">
+                <input type="submit" class="adminbtn" name="Delete" value="Delete">
+            </td>';
+        // PRINTS FORM CLOSE
+            print '</form>';
+        // PRINTS END TABLE ROW
+            print '</tr>';
+
+    } else {
+        
+        // PRINTING STARTING TABLE ROW
+            print '<tr>';
+        // PRINTING STARTING FORM
+            print '<form action="u_holidays.php" method="POST">';
+        // GETS/MAKES HIDDEN HOLIDAY ID
+            print '<input type="hidden" name="hid" value="'.$row["id"].'">';
+        // PRINTS HOLIDAY NAME AS TEXTBOX
+            print '<td><input type="text" class="aTextField" size="15" name="U_holidayname" value="'.$row["holidayname"].'"></td>';
+        // PRINTS HOLIDAY DATE AS TEXTBOX
+            print '<td><input type="text" class="aTextField" size="10" name="U_date" value="'.$row["date"].'"></td>';
+        // UPDATE BUTTON
+            print '
+            <td class="textcenter">
+                <input type="submit" class="adminbtn" name="Save" value="Save">
+                <input type="submit" class="adminbtn" name="Delete" value="Delete">
+            </td>';
+        // PRINTING CLOSE FORM
+            print '</form>';
+        // PRINTING END OF TABLE ROW
+            print '</tr>';
+    
+    
+}
+}
+
+// Frees the memory associated with a result
+$query_results->free();
+
+// close connection
+$mysqli->close();
+
+?>
+
+<!-- CLOSE FOR MAIN TABLE -->
 </table>
-  <!-- date picker javascript -->
-<script src="js/pikaday.js"></script>
-<script>
-    var picker = new Pikaday({ field: document.getElementById('date') });
-</script>
-<script src="js/pikaday.js"></script>
-<script>
-    var picker = new Pikaday({ field: document.getElementById('editdate') });
-</script>
-    </div>
-                    </div>
-<script>
-       $(document).ready(function() {
-           $('#TopHeader').delay(1500);
-           setTimeout(function() {
-               $('#TopHeader').removeClass();
-               $('#TopHeader .MyHeader').text('Update Holidays');
-           }, 1700);
-
-
-       });
-</script>
+  
+        
+        
+    </table>
 </body>
 </html>
