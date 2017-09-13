5b09c97668db1f3eb9f8157f5cd7c01282b52658
Badtz Maru <badtzmaru139@gmail.com>
2017-09-13 12:45:18 -0700

    simple link fix

--- statusview.php
+++ statusview.php
@@ -215,8 +215,9 @@
 								?>
 								<li>
 									<?php
-										echo $student['firstname'] . " " . substr($student['lastname'], 0, 1) . " ";
+										echo "<a style='text-decoration:none; color:black' href=viewreports.php?id=" . $student['studentid'] . ">" . $student['firstname'] . " " . substr($student['lastname'], 0, 1) . " ";
 										if ($status == "Offsite" || $status == "Late" || $status == "Field Trip") {echo $returntime_statusview;}
+										echo "</a>";
 									?>
 								</li>
 						<?php } ?>
