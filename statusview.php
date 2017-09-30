0b6f0a425abbad7fac4389220b6ecf95c7597d29
Milo Rupp <badtzmaru139@gmail.com>
2017-09-29 17:30:55 -0700

    removed inline css from links

--- statusview.php
+++ statusview.php
@@ -215,7 +215,7 @@
 								?>
 								<li>
 									<?php
-										echo "<a style='text-decoration:none; color:black' href=viewreports.php?id=" . $student['studentid'] . ">" . $student['firstname'] . " " . substr($student['lastname'], 0, 1) . " ";
+										echo "<a class='unstyled-link' href=viewreports.php?id=" . $student['studentid'] . ">" . $student['firstname'] . " " . substr($student['lastname'], 0, 1) . " ";
 										if ($status == "Offsite" || $status == "Late" || $status == "Field Trip") {echo $returntime_statusview;}
 										echo "</a>";
 									?>
